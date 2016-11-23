e58a90e9edeb7cc3b4fd47ac0aa2ffb3b01b96b7
Bilal Ahmad <ballimalik@live.co.uk>
2016-11-23 16:53:06 +0000

    Day16 Extercise 4 added...

---
+++ Day16/Task4/FileCopier.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class FileCopier{
+
+    public static void main(String[] args) {
+
+        FileCopier fc = new FileCopier();
+        if(args.length == 2){
+            fc.copy(args[0], args[1]);
+        } else {
+            System.out.println("USAGE: File copier <filename1> <filename2>");
+        }
+        
+    }
+
+    public void copy(String inputName, String outputName) {
+        try {
+            BufferedReader in = new BufferedReader(new FileReader( new File(inputName)));
+            PrintWriter out = new PrintWriter( new File (outputName));
+            String line;
+            while ((line = in.readLine()) != null) {
+                out.print(line);
+                out.println();
+            }
+            out.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("File " + inputName + " does not exist.");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
